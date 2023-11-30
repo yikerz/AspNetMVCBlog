40408f2d127b561add1fa23d82605313a52eace8
Travis LunMong <travis.lunmong@fdmgroup.com>
2023-11-30 23:35:25 +1100

    Create Domain Models

---
+++ Blog.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿/* 1. Create domain model */
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Blog.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        /* 2. Create props */
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        /* 3. Many-to-Many relation */
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ Blog.Web/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+﻿/* 1. Create domain model */
+namespace Blog.Web.Models.Domain
+{
+    public class Tag
+    {
+        /* 2. Create props */
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        /* 3. Many-to-Many relation */
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
