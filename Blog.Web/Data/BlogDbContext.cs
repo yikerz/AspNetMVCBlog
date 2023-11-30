0dbfd383f7a5c5d86d5ded053b4839caf70395b4
Travis LunMong <travis.lunmong@fdmgroup.com>
2023-12-01 01:20:41 +1100

    Adding ConnectionString To The Database in Appsetting.Json

--- Blog.Web/Data/BlogDbContext.cs
+++ Blog.Web/Data/BlogDbContext.cs
@@ -1,16 +1,13 @@
-﻿/* 5. Create DbContext with inheritance */
-using Blog.Web.Models.Domain;
+﻿using Blog.Web.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Web.Data
 {
     public class BlogDbContext : DbContext
     {
-        /* 6. Create constructor */
         public BlogDbContext(DbContextOptions options) : base(options)
         {
         }
-        /* 7. Create DbSet props */
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
     }
