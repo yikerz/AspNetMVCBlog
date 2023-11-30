24576b7159975f0e9a27ac2d20a3f120c8474ffa
Travis LunMong <travis.lunmong@fdmgroup.com>
2023-12-01 01:42:25 +1100

    Create New Controller (AdminTags), Add Action Method (GET) and Create New View

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,15 @@
+﻿/* 14. Create new controller */
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        /* 15. Create Add action method (GET) */
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
