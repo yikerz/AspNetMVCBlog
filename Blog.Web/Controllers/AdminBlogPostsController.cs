2fe3fe244f7b63fee1e5b4cb11bf38d64411938f
Travis LunMong <travis.lunmong@fdmgroup.com>
2023-12-01 12:03:34 +1100

    Create New HTML Form to Add New Blog

--- Blog.Web/Controllers/AdminBlogPostsController.cs
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -1,15 +1,35 @@
-﻿/* 57. Create new controller */
+﻿using Blog.Web.Models.Domain;
+using Blog.Web.Models.View;
+using Blog.Web.Views.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blog.Web.Controllers
 {
     public class AdminBlogPostsController : Controller
     {
-        /* 58. Create Add action method (GET) */
+        /* 65. Create constructor */
+        private readonly ITagRepository tagRepo;
+        public AdminBlogPostsController(ITagRepository tagRepo)
+        {
+            this.tagRepo = tagRepo;
+        }
         [HttpGet]
         public async Task<IActionResult> Add()
+        {
+            /* 66. Pass all tags into view */
+            var tags = await tagRepo.GetAllAsync();
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text=x.Name, Value=x.Id.ToString() })
+            };
+            return View(model);
+        }
+        /* 68. Create Add action method (POST) */
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
         { 
-            return View();
+            return RedirectToAction("Add");
         }
 
     }
